482dc5880bb12061dace2a8d3b1a94c5c52d4126
Mohamed Afsal <official.mohamedafsal@gmail.com>
2023-11-16 19:59:00 +0530

    Project completed with basic Fiori cannotations

---
+++ srv/risk-service.js
@@ -0,0 +1,18 @@
+const cds = require('@sap/cds')
+
+/**
+ * Implimentation for risk management service defined in ./risk-service.cds
+ */
+
+module.exports = cds.service.impl(async function () {
+    this.after('READ', 'Risks', risksData => {
+        const risks = Array.isArray(risksData) ? risksData : [risksData];
+        risks.forEach(risk => {
+            if (risk.impact >= 100000) {
+                risk.criticality = 1;
+            } else {
+                risk.criticality = 2;
+            }
+        });
+    });
+});
